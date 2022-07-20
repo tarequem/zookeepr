0baa73df345a7e8fe47729e691df2ff7e1b8bef6
tarequem <moatartareque@gmail.com>
2022-07-19 20:56:25 -0400

    MVP ready

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -22,8 +24,8 @@ app.get('/api/animals', (req, res) => {
     res.json(animals);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 app.get('/api/animals', (req, res) => {
@@ -73,3 +75,16 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  } 
+});
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
